be2aee72cf82e8f3e9279969a776e8ab0a91c281
Bames94 <89537533+Bames94@users.noreply.github.com>
2022-06-10 12:23:50 -0700

    Revert "initialize"
    This reverts commit d0c4134f5b8a8445468c5febd5eff1f164a1b9a0.

--- contracts/test/ERC20.sol
+++
@@ -1,9 +0,0 @@
-pragma solidity =0.5.16;
-
-import '../PolydexERC20.sol';
-
-contract ERC20 is PolydexERC20 {
-    constructor(uint _totalSupply) public {
-        _mint(msg.sender, _totalSupply);
-    }
-}
